40d5ba0203e7162d61e1f524c0756aedf909ccbd
Tyler Bakke <tylerbakke@gmail.com>
2016-05-05 11:54:02 -0700

    First layout

--- app/src/main/java/com/example/tylerbakke/myapplication/HomeActivity.java
+++ app/src/main/java/com/example/tylerbakke/myapplication/HomeActivity.java
@@ -1,11 +1,7 @@
 package com.example.tylerbakke.myapplication;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -14,9 +10,7 @@ public class HomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setContentView(R.layout.home);
     }
 
     @Override
